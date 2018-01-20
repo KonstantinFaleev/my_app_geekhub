f7d0d585bc1aa4aa5ca716abf3c1553e8ff2cdaa
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-20 23:19:43 +0200

    depot scafford

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,48 @@
 #AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 Product.delete_all
-
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Rails, Angular, Postgres, and Bootstrap',
                 description:
                     %{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-                image: 'ruby.jpg',
-                price: 49.95)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
+      As a Rails developer, you care about user experience and performance,
+      but you also want simple and maintainable code. Achieve all that by
+      embracing the full stack of web development, from styling with
+      Bootstrap, building an interactive user interface with AngularJS, to
+      storing data quickly and reliably in PostgreSQL. Take a holistic view of
+      full-stack development to create usable, high-performing applications,
+      and learn to use these technologies effectively in a Ruby on Rails
+      environment.
+      </p>},
+                image: 'dcbang.jpg',
+                price: 45.00)
+# . . .
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
                 description:
                     %{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-                image: 'ruby.jpg',
-                price: 49.95)
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question “Can we build this for ALL the devices?” with a
+      resounding YES. This book will help you get there with a real-world
+      introduction to seven platforms, whether you’re new to mobile or an
+      experienced developer needing to expand your options. Plus, you’ll find
+      out which cross-platform solution makes the most sense for your needs.
+      </p>},
+                image: '7apps.jpg',
+                price: 26.00)
+# . . .
 
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Ruby Performance Optimization',
                 description:
                     %{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-                image: 'ruby.jpg',
-                price: 49.95)
-
+      <em>Why Ruby Is Slow, and How to Fix It</em>
+      You don’t have to accept slow Ruby or Rails performance. In this
+      comprehensive guide to Ruby optimization, you’ll learn how to write
+      faster Ruby code—but that’s just the beginning. See exactly what makes
+      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
+      through perils of memory and CPU optimization, profiling, measuring,
+      performance testing, garbage collection, and tuning. You’ll find that
+      all those “hard” things aren’t so difficult after all, and your code
+      will run orders of magnitude faster.
+      </p>},
+                image: 'adrpo.jpg',
+                price: 46.00)
