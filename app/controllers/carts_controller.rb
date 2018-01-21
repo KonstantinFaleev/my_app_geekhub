8c762caf998ab9c9b91291103664d4ff6bbdf41a
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-21 18:23:29 +0200

    Handling Errors

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,16 @@
 class CartsController < InheritedResources::Base
+  #before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   private
 
     def cart_params
       params.require(:cart).permit()
+    end
+
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_index_url, notice: 'Invalid cart'
     end
 end
 
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,7 +23,7 @@ class LineItemsController < InheritedResources::Base
   private
 
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
 
