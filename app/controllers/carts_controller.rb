8a34fa59bdd8c3212fb8c2a031e5ba003eb9ed9f
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-21 19:00:12 +0200

    Finishing the Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,16 @@ class CartsController < InheritedResources::Base
   #before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
+  def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
+    respond_to do |format|
+      format.html { redirect_to store_index_url,
+                                notice: 'Your cart is currently empty' }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
     def cart_params
