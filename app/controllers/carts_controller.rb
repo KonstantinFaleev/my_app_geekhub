e11ca3c52c9a23104bd24316cab08a1837feacd2
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-21 23:38:21 +0200

    Add a Dash of Ajax

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,24 @@ class CartsController < InheritedResources::Base
   #before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
+  def index
+    @carts = Cart.all
+  end
+
+  # GET /carts/new
+  def new
+    @cart = Cart.new
+  end
+
+  # GET /carts/1/edit
+  def edit
+  end
+
   def destroy
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_index_url,
-                                notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
